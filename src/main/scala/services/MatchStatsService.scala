61ca5043bdc6690b20c1eaed1a8eed96d12a65ef
callum-gibbons1 <callum.gibbons@imgarena.com>
2023-12-11 00:30:05 +0000

    Rest of changes

--- src/main/scala/services/MatchStatsService.scala
+++ src/main/scala/services/MatchStatsService.scala
@@ -1,18 +1,53 @@
 package services
 
-import models.{FutsalEvent, FutsalMatch, FutsalPlayer, Goal}
+import models.{FutsalEvent, FutsalMatch, FutsalMatchEvent, FutsalPlayer, Goal, KickOff, Pass, Tackle}
 
 class MatchStatsService(futsalMatch: FutsalMatch, matchState: MatchState) {
-  def topGoalScorer: FutsalPlayer = {
-    getAllGoals.groupBy{
-      case Goal(scorer) => scorer
-    }
-      .maxBy(_._2.size)
-      ._1
-  }
+	def topGoalScorer = {
+		getAllGoals.groupBy{
+			case Goal(scorer) => scorer
+		}
+			.maxByOption(playerAndGoals => {
+				val (player, goals) = playerAndGoals
+				goals.size
+			})
+			.map(_._1)
+	}
 
-  private lazy val getAllGoals: List[FutsalEvent] =
-    matchState.matchEvents.filterNot {
-      case Goal(_) => true
-    }
+	def topTackler: FutsalPlayer = {
+		def calculateTackleCounts(acc: Map[FutsalPlayer, Int], restOfEvents: List[FutsalMatchEvent]): Map[FutsalPlayer, Int] = {
+			restOfEvents match {
+				case head :: tail =>
+					head match {
+						case Tackle(attacker) =>
+							val newCount = acc.get(attacker)
+								.map(count => count + 1)
+
+							newCount match {
+								case Some(nc) =>
+									val newAcc = acc.removed(attacker) + (attacker -> nc)
+									calculateTackleCounts(newAcc, tail)
+								case None =>
+									val newAcc = acc + (attacker -> 1)
+									calculateTackleCounts(newAcc, tail)
+							}
+						case _ => calculateTackleCounts(acc, tail)
+					}
+				case Nil => acc
+			}
+		}
+
+		calculateTackleCounts(Map.empty, matchState.matchEvents)
+			.maxBy(playerAndTackles => {
+				val (_, tackles) = playerAndTackles
+				tackles
+			})
+			._1
+	}
+
+	private lazy val getAllGoals: List[FutsalMatchEvent] =
+		matchState.matchEvents.filter {
+			case Goal(_) => true
+			case _ => false
+		}
 }
--- src/test/scala/utils/TestUtils.scala
+++ src/test/scala/utils/TestUtils.scala
@@ -1,56 +1,56 @@
 package utils
 
-import models.{FutsalMatch, FutsalPlayer, FutsalTeam, Goal, KickOff, Pass, Tackle, Undo}
+import models._
 
 object TestUtils {
-  val testTeam1Player1 = FutsalPlayer(1, "Harry")
-  val testTeam1Player2 = FutsalPlayer(2, "Brandon")
-  val testTeam1Player3 = FutsalPlayer(3, "Jon")
-  val testTeam1Player4 = FutsalPlayer(4, "Alex")
-  val testTeam1Player5 = FutsalPlayer(5, "Jude")
-  val testTeam1Players = List(
-    testTeam1Player1,
-    testTeam1Player2,
-    testTeam1Player3,
-    testTeam1Player4,
-    testTeam1Player5,
-  )
-  val testTeam2Player1 = FutsalPlayer(6, "Colm")
-  val testTeam2Player2 = FutsalPlayer(7, "Juan")
-  val testTeam2Player3 = FutsalPlayer(8, "Mustafa")
-  val testTeam2Player4 = FutsalPlayer(9, "Nigel")
-  val testTeam2Player5 = FutsalPlayer(10, "Santiago")
-  val testTeam2Players = List(
-    testTeam2Player1,
-    testTeam2Player2,
-    testTeam2Player3,
-    testTeam2Player4,
-    testTeam2Player5,
-  )
+	val testTeam1Player1 = FutsalPlayer(1, "Harry")
+	val testTeam1Player2 = FutsalPlayer(2, "Brandon")
+	val testTeam1Player3 = FutsalPlayer(3, "Jon")
+	val testTeam1Player4 = FutsalPlayer(4, "Alex")
+	val testTeam1Player5 = FutsalPlayer(5, "Jude")
+	val testTeam1Players = List(
+		testTeam1Player1,
+		testTeam1Player2,
+		testTeam1Player3,
+		testTeam1Player4,
+		testTeam1Player5,
+	)
+	val testTeam2Player1 = FutsalPlayer(6, "Colm")
+	val testTeam2Player2 = FutsalPlayer(7, "Juan")
+	val testTeam2Player3 = FutsalPlayer(8, "Mustafa")
+	val testTeam2Player4 = FutsalPlayer(9, "Nigel")
+	val testTeam2Player5 = FutsalPlayer(10, "Santiago")
+	val testTeam2Players = List(
+		testTeam2Player1,
+		testTeam2Player2,
+		testTeam2Player3,
+		testTeam2Player4,
+		testTeam2Player5,
+	)
 
-  val testFutsalMatch = FutsalMatch(
-    team1 = FutsalTeam("Players United", testTeam1Players),
-    team2 = FutsalTeam("Team FC", testTeam2Players)
-  )
+	val testFutsalMatch = FutsalMatch(
+		team1 = FutsalTeam("Players United", testTeam1Players),
+		team2 = FutsalTeam("Team FC", testTeam2Players)
+	)
 
-  val testEvents = List(
-    KickOff(testTeam1Player1),
-    Pass(testTeam1Player4),
-    Pass(testTeam1Player2),
-    Pass(testTeam1Player3),
-    Tackle(testTeam2Player1),
-    Pass(testTeam2Player2),
-    Pass(testTeam2Player4),
-    Goal(testTeam2Player2),
-    KickOff(testTeam1Player1),
-    Pass(testTeam1Player4),
-    Tackle(testTeam2Player4),
-    Pass(testTeam2Player2),
-    Tackle(testTeam1Player3),
-    Goal(testTeam1Player3),
-    KickOff(testTeam2Player3),
-    Pass(testTeam2Player1),
-    Pass(testTeam2Player3),
-    Goal(testTeam1Player3),
-  )
+	val testEvents = List(
+		KickOff(testTeam1Player1),
+		Pass(testTeam1Player4),
+		Pass(testTeam1Player2),
+		Pass(testTeam1Player3),
+		Tackle(testTeam2Player1),
+		Pass(testTeam2Player2),
+		Pass(testTeam2Player4),
+		Goal(testTeam2Player2),
+		KickOff(testTeam1Player1),
+		Pass(testTeam1Player4),
+		Tackle(testTeam2Player4),
+		Pass(testTeam2Player2),
+		Tackle(testTeam1Player3),
+		Goal(testTeam1Player3),
+		KickOff(testTeam2Player3),
+		Pass(testTeam2Player1),
+		Pass(testTeam2Player3),
+		Goal(testTeam1Player3),
+	)
 }
